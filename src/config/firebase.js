bf16fb9573d2ff1d12a82cd35c69e4ad080c34aa
Mark Arenillo <56244509+markarenillo@users.noreply.github.com>
2021-05-24 14:45:20 +0800

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -19,27 +19,3 @@ measurementId: process.env.VUE_APP_AUTH_FIREBASE_MEASUREMENT_ID
 
 export default firebase
 
-// export default {
-//     auth: firebase.auth(),
-//     login() {
-//       const provider = new firebase.auth.GoogleAuthProvider();
-//       firebase.auth().signInWithPopup(provider)
-//       .then(function(result) {
-//         console.log(result);
-//       })
-//       .catch(function(error){
-//         const errorCode = error.code;
-//         const errorMessage = error.message;
-//         const email = error.email;
-//         const credential = error.credential;
-//         console.log(errorCode, errorMessage, email, credential);
-//         })
-//     },
-//     logout() {
-//       firebase.auth().signOut()
-//       .then(function() {})
-//       .catch(function(error) {
-//         console.log(error)});
-//     }
-// }
-
